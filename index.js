548e5d968df73cb2020798b5ae79e037e4ce1e24
Harshith-reddy1629 <reddybharshith3@gmail.com>
2023-09-15 05:54:23 +0000

    commit 1

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 8001;
 
 let database = null;
+
 const initializeDbAndServer = async () => {
   try {
     database = await open({
@@ -41,102 +42,11 @@ const initializeDbAndServer = async () => {
 };
 
 initializeDbAndServer();
-// @user Details
-
-app.get("/users/onlyadmin/get/", async (req, res) => {
-  const Q = `
-  SELECT * FROM users;`;
-
-  const RunQ = await database.all(Q);
-
-  res.status(200).send(RunQ);
-});
-
-// @User registration
-
-// @PasswordValidation
-const PasswordValidation = (request, response, next) => {
-  const { password } = request.body;
-  if (password.length < 8) {
-    response.status(400).send({
-      errMsg: "Password should have minimum 8 characters",
-    });
-  } else {
-    next();
-  }
-};
-
-// @MailValidation
-const MailValidation = async (request, response, next) => {
-  const { email } = request.body;
-
-  const selectUserQuery = `SELECT * FROM users WHERE email = '${email}'`;
-
-  const dbUser = await database.get(selectUserQuery);
-
-  if (dbUser === undefined) {
-    next();
-  } else {
-    response.status(400).send({
-      errMsg: "email already exists",
-    });
-  }
-};
-
-// @user Post METHOD
-app.post(
-  "/users/",
-  PasswordValidation,
-  MailValidation,
-  async (request, response) => {
-    const { username, name, password, email } = request.body;
-
-    const hashedPassword = await bcrypt.hash(request.body.password, 10);
-    const selectUserQuery = `SELECT * FROM users WHERE username = '${username}'`;
-    const dbUser = await database.get(selectUserQuery);
-    if (dbUser === undefined) {
-      const createUserQuery = `
-        INSERT INTO 
-          users (username, name, password, email) 
-        VALUES 
-          (
-            '${username}', 
-            '${name}',
-            '${hashedPassword}', 
-            '${email}'
-          )`;
-      const dbResponse = await database.run(createUserQuery);
-      const newUserId = dbResponse.lastID;
-      response.send(dbResponse);
-    } else {
-      response
-        .status(400)
-        .send({ errMsg: "User already exists with this username" });
-    }
-  }
-);
-
-// @user delete
-
-// @user delete method
-app.delete("/users/:id/", async (request, response) => {
-  const { id } = request.params;
-
-  const Q = `
-DELETE FROM
-users
-WHERE
-id =${id};`;
-
-  await database.run(Q);
-
-  response.send("done");
-});
-
-// PROJECTS DB
 
 // @project post method
+
 // Only Admin
+
 app.post("/projects/onlyadmin/canpost/", async (req, res) => {
   const { projectImg, projectName, description, projectLink } = req.body;
 
@@ -155,6 +65,7 @@ VALUES (
 });
 
 // @projects get method
+
 app.get("/projects/", async (req, res) => {
   const QueryToRun = `
   SELECT * FROM projects_table;
